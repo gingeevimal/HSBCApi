e72b4486fc1736c8628985e8f603e6fbf24ba1b6
vimal kumar <gingeevimal@outlook.com>
2021-08-11 17:09:41 +0530

    SAML POC draft

--- Services/CAD/CAD.API/Controllers/CAdocumentController.cs
+++ Services/CAD/CAD.API/Controllers/CAdocumentController.cs
@@ -9,18 +9,32 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Saml;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.WsFederation;
+using System.Net.Http;
+using RestSharp;
+using System.Xml;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
 
 namespace Ordering.API.Controllers
 {
+    //[Authorize]
     [ApiController]
     [Route("api/v1/[controller]")]
     public class CAdocumentController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private IWebHostEnvironment _hostEnvironment;
 
-        public CAdocumentController(IMediator mediator)
+
+        public CAdocumentController(IMediator mediator, IWebHostEnvironment environment)
         {
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _hostEnvironment = environment;
+
         }
 
         //[HttpGet("{userName}", Name = "GetCAdocument")]
@@ -67,6 +81,106 @@ namespace Ordering.API.Controllers
             var command = new DeleteCAdocumentCommand() { Id = id };
             await _mediator.Send(command);
             return NoContent();
+        }
+        [HttpGet]
+        [Route("Login")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public ActionResult Login()
+        {
+           // SamlConsume();
+            //TODO: specify the SAML provider url here, aka "Endpoint"
+            //var samlEndpoint = "http://saml-provider-that-we-use.com/login/";
+            var samlEndpoint = "https://login.microsoftonline.com/b9165cc7-82cb-4b2f-86eb-f60b7e3809a5/saml2";
+
+            var request = new AuthRequest(
+                "https://hoekstra.dev/SAMLBlogPost", //TODO: put your app's "entity ID" here
+                "https://localhost:44373/api/v1/CAdocument/SamlConsume" //TODO: put Assertion Consumer URL (where the provider should redirect users after authenticating)
+                //"http://localhost:4200/" 
+                );
+            //var request = new AuthRequest(
+            //"http://www.myapp.com", //TODO: put your app's "entity ID" here
+            //"http://www.myapp.com/SamlConsume" //TODO: put Assertion Consumer URL (where the provider should redirect users after authenticating)
+            //);
+            var redirectUrl = request.GetRedirectUrl(samlEndpoint);
+
+            HttpClient client = new HttpClient();
+
+            // Send a request asynchronously continue when complete 
+            client.GetAsync(redirectUrl).ContinueWith(
+                (requestTask) =>
+                {
+        // Get HTTP response from completed task. 
+        HttpResponseMessage response = requestTask.Result;
+
+        // Check that response was successful or throw exception 
+        response.EnsureSuccessStatusCode();
+
+        // Read response asynchronously as JsonValue
+        //response.Content.ReadAsAsync<JsonArray>().ContinueWith(
+        //                        (readTask) =>
+        //                        {
+        //                            var result = readTask.Result
+        //                            //Do something with the result                   
+        //            });
+                });
+            //redirect the user to the SAML provider
+            return Redirect(request.GetRedirectUrl(redirectUrl));
+            //return Challenge(
+            //    new AuthenticationProperties { RedirectUri = redirectUrl },
+            //    WsFederationDefaults.AuthenticationScheme);
+        }
+        [HttpPost]
+        [Route("SamlConsume")]
+        public object SamlConsume()
+        {
+
+             //https://github.com/jitbit/AspNetSaml    POC Link
+
+            // 1. TODO: specify the certificate that your SAML provider gave you
+
+            string path = Path.Combine(_hostEnvironment.ContentRootPath, "SAMLBlogpost.xml");
+
+            XmlDocument _LocalInfo_Xml = new XmlDocument();
+            _LocalInfo_Xml.Load(path);
+            XmlElement _XmlElement;
+            _XmlElement = _LocalInfo_Xml.GetElementsByTagName("X509Certificate")[0] as XmlElement;
+            string samlCertificate = _XmlElement.InnerText;
+
+            // 2. Let's read the data - SAML providers usually POST it into the "SAMLResponse" var
+            //Saml.Response samlResponse = new Response(samlCertificate, Request.Form["SAMLResponse"]);
+            string Samlresponse = Request.Form["Samlresponse"];
+            Response samlResponse = new Response(samlCertificate, Samlresponse);
+
+            // 3. We're done!
+            if (samlResponse.IsValid())
+            {
+                //WOOHOO!!! user is logged in
+
+                //Some more optional stuff for you
+                //let's extract username/firstname etc
+                string username, email, firstname, lastname;
+                try
+                {
+                    username = samlResponse.GetNameID();
+                    email = samlResponse.GetEmail();
+                    firstname = samlResponse.GetFirstName();
+                    lastname = samlResponse.GetLastName();
+                    return Ok(username);
+                }
+                
+                catch (Exception ex)
+                {
+                    //insert error handling code
+                    //no, really, please do
+                    return null;
+                }
+
+                //user has been authenticated, put your code here, like set a cookie or something...
+                //or call FormsAuthentication.SetAuthCookie() or something
+            }
+            return Ok(string.Empty);
         }
     }
 }
--- Services/CAD/CAD.API/Startup.cs
+++ Services/CAD/CAD.API/Startup.cs
@@ -1,5 +1,7 @@
 //using EventBus.Messages.Common;
 using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.WsFederation;
 //using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
@@ -12,6 +14,8 @@ using Microsoft.OpenApi.Models;
 using Ordering.Application;
 using Ordering.Infrastructure;
 using Ordering.Infrastructure.Persistence;
+using Sustainsys.Saml2;
+using Sustainsys.Saml2.Metadata;
 
 namespace Ordering.API
 {
@@ -48,6 +52,43 @@ namespace Ordering.API
 
             // General Configuration
             //  services.AddScoped<BasketCheckoutConsumer>();
+            //           services.AddAuthentication(sharedOptions =>
+            //           {
+            //               sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            //               sharedOptions.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            //               sharedOptions.DefaultChallengeScheme = "Saml2";
+            //           }).AddSaml2(options =>
+            //{
+            //    //options.SPOptions.EntityId = new EntityId("https://localhost:44342/Saml2");
+            //    //options.SPOptions.EntityId = new EntityId("https://localhost:44373/api/v1/CAdocument/GetCAdocument?businesspartner=test");
+            //    options.SPOptions.EntityId = new EntityId("https://localhost:44373/api/v1/CAdocument/GetCAdocument");
+            //    options.IdentityProviders.Add(
+            //     new IdentityProvider(
+            //       //new EntityId("https://sts.windows.net/63eb1bcb-f74f-4703-8243-6f73d78ebf52/"), options.SPOptions)
+            //       new EntityId("https://sts.windows.net/b9165cc7-82cb-4b2f-86eb-f60b7e3809a5/"), options.SPOptions)
+            //    {
+            //        MetadataLocation = "https://login.microsoftonline.com/b9165cc7-82cb-4b2f-86eb-f60b7e3809a5/federationmetadata/2007-06/federationmetadata.xml?appid=042b32b5-ee63-414c-96e5-fd41a8abde54"
+            //     });
+            //})
+            //.AddCookie();
+
+            services.AddAuthentication(sharedOptions =>
+            {
+                sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultChallengeScheme = WsFederationDefaults.AuthenticationScheme;
+            })
+       .AddWsFederation(options =>
+       {
+           // this is where your AppID URI goes
+          // options.Wtrealm = "https://microsoft.onmicrosoft.com/5b60c247-398e-4c00-9ba4-a8xxxxxxxx4d1";
+           options.Wtrealm = "api://79d17639-e1f0-4b4d-9be5-624bf7738697";
+           //options.MetadataAddress = "https://login.microsoftonline.com/72f988bf-xxxx-41af-91ab-xxxxxxxxxxdb47/federationmetadata/2007-06/federationmetadata.xml";
+           options.MetadataAddress = "https://login.microsoftonline.com/b9165cc7-82cb-4b2f-86eb-f60b7e3809a5/federationmetadata/2007-06/federationmetadata.xml";
+       })
+       .AddCookie();
+
+            services.AddMvc();
             services.AddAutoMapper(typeof(Startup));
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -80,6 +121,7 @@ namespace Ordering.API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ordering.API v1"));
             }
             app.UseCors("CorsPolicy");
+            app.UseAuthentication();
 
             app.UseRouting();
 
